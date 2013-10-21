30fe43b766c2b04ff5c532bdeeb13dd6640d5dad
Zach Supalla <zsupalla@gmail.com>
2013-10-20 19:30:23 -0500

    Basic javascript events are happening

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,4 +1,20 @@
-$('#led').on('click', function(event) {
-  event.preventDefault;
-  console.log("LED toggled");
-})
+(function ($) {
+  $(document).ready(
+    function(){
+      $('#led').on('click', function(event) {
+        event.preventDefault();
+        console.log("LED toggled");
+      });
+
+      $('#buzzer').on('click', function(event) {
+        event.preventDefault();
+        console.log("Buzzer toggled");
+      });
+
+      $('#vibrate').on('click', function(event) {
+        event.preventDefault();
+        console.log("Vibrate toggled");
+      });
+    }
+  )
+})(window.jQuery)
